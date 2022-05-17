8b7ee51cd5c827d7e68537821c67d26dd24840a6
Tobias <tobias@fixmycity.de>
2022-05-17 16:09:23 +0200

    PresetSlider: Replace inline svg with import

--- src/components/Homepage/PresetSlider.tsx
+++ src/components/Homepage/PresetSlider.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
 import classNames from 'classnames';
 import useEmblaCarousel from 'embla-carousel-react';
 import React, { useCallback, useEffect, useState } from 'react';
@@ -82,37 +83,14 @@ export const PresetSlider: React.FC<Props> = ({ slides }) => {
         className={classNames({ 'bg-red-400': allowPrev })}
         onClick={scrollPrev}
       >
-        {/* https://heroicons.com/ */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15 19l-7-7 7-7"
-          />
-        </svg>
+        <ChevronLeftIcon className="h-8 w-8" />
       </button>
       <button
         type="button"
         className={classNames({ 'bg-red-400': allowNext })}
         onClick={scrollNext}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-        </svg>
+        <ChevronRightIcon className="h-8 w-8" />
       </button>
     </div>
   );
