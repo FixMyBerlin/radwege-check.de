8d60ec6cee1539a8739f995b40a3c87007f25d7c
Tobias <tobias@fixmycity.de>
2022-05-17 17:34:47 +0200

    Introduce specific types for scene results

--- src/components/Scenes/types.ts
+++ src/components/Scenes/types.ts
@@ -13,7 +13,59 @@ export type ResultBucketProps = {
   selected: boolean;
 };
 
-export type ResultItemProps = { [key: string]: number | string };
+export type ResultItemProps = {
+  sceneId: string;
+  sceneIdCar: string;
+  sceneIdPedestrian: string;
+  sceneIdCount: number;
+
+  bicycleLaneSurface: string;
+  bicycleLaneUsableWidthNumber: number | undefined;
+  bicycleLaneWidth: string;
+  bicycleLaneWidthNumber: number | undefined;
+  bufferHasPhysicalProtection: boolean;
+  bufferLeftMarking: string;
+  bufferLeftPhysicalProtection: string;
+  bufferLeftWidth: string;
+  bufferLeftWidthNumber: number | undefined;
+  bufferRightMarking: string;
+  bufferRightWidth: string;
+  bufferRightWidthNumber: number | undefined;
+  leftOfBicycleLane: string;
+  location: string;
+  parking: string;
+  pavementHasShops: boolean;
+  pavementWidth: string;
+  pavementWidthNumber: number | undefined;
+  pointOfView: string;
+  vehicleLaneMaxspeed: string;
+  vehicleLaneUsage: string;
+  vehicleTrafficVolume: string;
+
+  vote0Unsafe: number;
+  vote1RatherUnsafe: number;
+  vote2Save: number;
+  voteCar3VerySave: number;
+  vote3VerySave: number;
+  voteCount: number;
+  voteMeans: number;
+  voteScore: number;
+  voteCar0Unsafe: number | undefined;
+  voteCar1RatherUnsafe: number | undefined;
+  voteCar2Save: number | undefined;
+  voteCarCount: number | undefined;
+  voteCarMeans: number | undefined;
+  voteCarScore: number | undefined;
+  votePedestrian0Unsafe: number | undefined;
+  votePedestrian1RatherUnsafe: number | undefined;
+  votePedestrian2Save: number | undefined;
+  votePedestrian3VerySave: number | undefined;
+  votePedestrianCount: number | undefined;
+  votePedestrianMeans: number | undefined;
+  votePedestrianScore: number | undefined;
+
+  path: string;
+};
 
 export type ResultItemsProps = Array<ResultItemProps>;
 
