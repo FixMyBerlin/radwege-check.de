0d2264cc359c99349407f4eea2a1d4f4462d1a3e
Tobias <tobias@fixmycity.de>
2022-05-19 14:28:45 +0200

    SceneLayout: One way to fix overscroll (the old way)

--- src/components/Layout/FixedLayout.tsx
+++ src/components/Layout/FixedLayout.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export const FixedLayout: React.FC = ({ children }) => {
+  /*
+    About those Helmet `bodyAttributes`:
+    - The classes prevent overscroll https://stackoverflow.com/a/24727206/729221
+    - Helmet is the way to set classes in Gatsby https://stackoverflow.com/a/46405558/729221
+  */
   return (
-    <div className="flex h-screen w-screen flex-col">
-      <main className="flex-grow">{children}</main>
-    </div>
+    <>
+      <Helmet
+        bodyAttributes={{
+          class: 'lg:absolute overflow-hidden fixed',
+        }}
+      />
+      <main className="h-screen w-screen">{children}</main>
+    </>
   );
 };
