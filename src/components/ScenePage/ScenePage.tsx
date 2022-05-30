540b33efbc0d0a9abe760fe44f8d1b1f5bc00e8f
Tobias <tobias@fixmycity.de>
2022-05-30 17:07:39 +0200

    ScenePage: Update with latest wording and some styles

--- src/components/ScenePage/ScenePage.tsx
+++ src/components/ScenePage/ScenePage.tsx
@@ -1,12 +1,14 @@
+import { InformationCircleIcon } from '@heroicons/react/outline';
 import classNames from 'classnames';
 import React from 'react';
+import { Link } from '../Link';
 import { SceneImage } from '../Scenes';
 import { ResultCells } from '../Scenes/Results/ResultCells';
 import { ResultStackedBarchart } from '../Scenes/Results/ResultStackedBarchart';
 import {
+  SceneCategory,
   ScenePrimaryProps,
   SceneSecondaryProps,
-  SceneCategory,
 } from '../Scenes/types';
 import { dataTable } from './dataTable';
 
@@ -26,43 +28,66 @@ export const ScenePage: React.FC<Props> = ({ category, scene }) => {
         Szene <code>{scene.sceneId}</code>
       </h1>
       <div className="flex max-w-7xl flex-col gap-6 lg:grid lg:grid-cols-4">
-        <div className="order-2 rounded bg-blue-50 p-6 lg:order-none">
-          <p>Straßenklasse: {categoryTranslation}</p>
-          <ResultCells category={category} scene={scene} />
+        <div className="order-2 lg:order-none">
+          <p className="px-6 pt-0 pb-3">
+            <strong className="text-xxs font-semibold">Straßenklasse</strong>
+            <br />
+            {categoryTranslation}
+          </p>
+          <div className="rounded bg-blue-50 p-6">
+            <ResultCells category={category} scene={scene} />
+          </div>
         </div>
+
         <div className="order-1 col-span-2 lg:order-none">
           <SceneImage
             sceneId={scene.sceneId}
             className="mb-5 h-96 w-full rounded object-cover object-bottom"
           />
           <div className="grid grid-cols-2 gap-5 text-xs lg:text-base">
-            {'sceneIdPedestrian' in scene ? (
-              <div>
-                Blickwinkel einer Fußgänger:in
+            {'sceneIdPedestrian' in scene && scene.sceneIdPedestrian ? (
+              <figure>
+                <figcaption className="mb-0.5 font-normal">
+                  Perspektive einer Fußgänger:in
+                </figcaption>
                 <SceneImage
                   sceneId={scene.sceneIdPedestrian}
                   alt="Illustration der bewerteten Szene aus Blickwinkel einer Fußgänger:in"
                   className="w-full rounded object-cover"
                 />
-              </div>
+              </figure>
             ) : (
               <div className="rounded bg-gray-50" />
             )}
-            {'sceneIdCar' in scene ? (
-              <div>
-                Blickwinkel einer Autofahrer:in
+            {'sceneIdCar' in scene && scene.sceneIdCar ? (
+              <figure>
+                <figcaption className="mb-0.5 font-normal">
+                  Perspektive einer Autofahrer:in
+                </figcaption>
                 <SceneImage
                   sceneId={scene.sceneIdCar}
                   alt="Illustration der bewerteten Szene aus Blickwinkel einer Autofahrer:in"
                   className="w-full rounded object-cover"
                 />
-              </div>
+              </figure>
             ) : (
               <div className="rounded bg-gray-50" />
             )}
           </div>
         </div>
+
         <div className="order-3 flex h-96 flex-col lg:order-none">
+          <p className="mb-5 flex items-center">
+            Durchschnittliche Bewertungen dieser Szene zur subjektiven
+            Sicherheit{' '}
+            <Link
+              to="https://fixmyberlin.de/research/subjektive-sicherheit"
+              external
+              blank
+            >
+              <InformationCircleIcon className="h-6 w-6" />
+            </Link>
+          </p>
           <ResultStackedBarchart
             scene={scene}
             classNameBarchartHeight="h-full"
