15b24f386e07cf7bc463051a3a8e5e057ef9c6a4
Tobias <tobias@fixmycity.de>
2022-05-31 17:39:49 +0200

    Netlify build debug 'utils/domain'

--- src/components/utils/domain.ts
+++ src/components/utils/domain.ts
@@ -8,3 +8,11 @@ export const domain =
   process.env.CONTEXT === 'production'
     ? process.env.URL
     : process.env.DEPLOY_PRIME_URL || 'https://radwege-check.de';
+
+// Let's see what the Netlify build log tells us…
+console.log({
+  context: process.env.CONTEXT,
+  url: process.env.URL,
+  prime_url: process.env.DEPLOY_PRIME_URL,
+  env: process.env,
+});
