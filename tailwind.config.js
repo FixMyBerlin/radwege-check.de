61250fb8b6e7776240dbd056a704305d765337b8
Tobias <tobias@fixmycity.de>
2022-05-31 14:29:40 +0200

    Tailwind: Remove unused typography config

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,33 +11,13 @@ module.exports = {
         innerGray: 'inset 1px 1px 3px 0 rgb(209 213 219)', // gray-300
       },
       fontSize: { xxs: '0.7rem' },
-      typography: ({ theme }) => ({
-        pink: {
-          css: {
-            '--tw-prose-body': theme('colors.slate[800]'),
-            '--tw-prose-headings': theme('colors.slate[900]'),
-            '--tw-prose-lead': theme('colors.slate[700]'),
-            '--tw-prose-links': theme('colors.emerald[400]'),
-            '--tw-prose-bold': theme('colors.slate[900]'),
-            '--tw-prose-counters': theme('colors.slate[600]'),
-            '--tw-prose-bullets': theme('colors.slate[400]'),
-            '--tw-prose-hr': theme('colors.slate[300]'),
-            '--tw-prose-quotes': theme('colors.slate[900]'),
-            '--tw-prose-quote-borders': theme('colors.emerald[400]'),
-            '--tw-prose-captions': theme('colors.slate[700]'),
-            '--tw-prose-code': theme('colors.slate[900]'),
-            '--tw-prose-pre-code': theme('colors.slate[100]'),
-            '--tw-prose-pre-bg': theme('colors.slate[900]'),
-            '--tw-prose-th-borders': theme('colors.slate[300]'),
-            '--tw-prose-td-borders': theme('colors.slate[200]'),
-          },
       typography: {
         DEFAULT: {
           // Tell the plugin to not add any classes to anchor tags
           // since we always use the Link component which is styled already.
           css: { a: false },
         },
-      }),
+      },
     },
     fontFamily: {
       sans: ['Barlow', 'Segoe UI', 'Tahoma', 'sans-serif'],
