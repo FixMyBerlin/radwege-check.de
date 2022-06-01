9061ab7caad214447a34db9e42b8a21013b61072
Tobias <tobias@fixmycity.de>
2022-06-01 10:12:07 +0200

    NotFound + CommingSoon: Improve mobile

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,36 +1,33 @@
 import React from 'react';
-import { Layout, MetaTags } from '~/components/Layout';
+import { LayoutArticle, MetaTags } from '~/components/Layout';
 import { Link } from '~/components/Link';
 
 const NotFound: React.FC = () => {
   return (
-    <Layout>
+    <LayoutArticle>
       <MetaTags noindex title="404 | Seite nicht gefunden." />
-      <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
-        <div className="mx-auto max-w-max">
-          <main className="sm:flex">
-            <p className="text-4xl font-extrabold text-orange-400 sm:text-5xl">
-              404
+
+      <div className="sm:flex">
+        <p className="text-4xl font-extrabold text-orange-400 sm:text-5xl">
+          404
+        </p>
+        <div className="sm:ml-6">
+          <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1 className="text-4xl font-extrabold tracking-tight text-slate-900 sm:text-5xl">
+              Page not found
+            </h1>
+            <p className="mt-1 text-base text-slate-500">
+              Please check the URL in the address bar and try again.
             </p>
-            <div className="sm:ml-6">
-              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-                <h1 className="text-4xl font-extrabold tracking-tight text-slate-900 sm:text-5xl">
-                  Page not found
-                </h1>
-                <p className="mt-1 text-base text-slate-500">
-                  Please check the URL in the address bar and try again.
-                </p>
-              </div>
-              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-                <Link button to="/">
-                  Startseite
-                </Link>
-              </div>
-            </div>
-          </main>
+          </div>
+          <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+            <Link button to="/">
+              Startseite
+            </Link>
+          </div>
         </div>
       </div>
-    </Layout>
+    </LayoutArticle>
   );
 };
 
--- src/pages/CommingSoon.tsx
+++ src/pages/CommingSoon.tsx
@@ -1,42 +1,39 @@
 import React from 'react';
-import { Layout, MetaTags } from '~/components/Layout';
+import { LayoutArticle, MetaTags } from '~/components/Layout';
 import { Link } from '~/components/Link';
 import { NewsletterWidget } from '~/components/NewsletterWidget';
 
 const CommingSoon: React.FC = () => {
   return (
-    <Layout hideBetaModal>
+    <LayoutArticle hideBetaModal>
       <MetaTags
         noindex
         title="Radwege-Check | Diese Seite ist noch in Arbeit."
       />
-      <div className="min-h-full bg-white px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
-        <div className="mx-auto max-w-max">
-          <main className="sm:flex">
-            <p className="text-4xl font-extrabold text-orange-400 sm:text-5xl">
-              404
+
+      <div className="sm:flex">
+        <p className="text-4xl font-extrabold text-orange-400 sm:text-5xl">
+          404
+        </p>
+        <div className="sm:ml-6">
+          <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1 className="text-4xl font-extrabold tracking-tight text-slate-900 sm:text-5xl">
+              Diese Seite ist noch in Arbeit.
+            </h1>
+            <p className="mt-1 mb-5 text-base text-slate-500">
+              Bitte abonniere unseren Newsletter. Wir informieren dich, sobald
+              neue Funktionen verfügbar sind.
             </p>
-            <div className="sm:ml-6">
-              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-                <h1 className="text-4xl font-extrabold tracking-tight text-slate-900 sm:text-5xl">
-                  Diese Seite ist noch in Arbeit.
-                </h1>
-                <p className="mt-1 mb-5 text-base text-slate-500">
-                  Bitte abonniere unseren Newsletter. Wir informieren dich,
-                  sobald neue Funktionen verfügbar sind.
-                </p>
-                <NewsletterWidget />
-              </div>
-              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-                <Link button to="/">
-                  Startseite
-                </Link>
-              </div>
-            </div>
-          </main>
+            <NewsletterWidget className="w-full" />
+          </div>
+          <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+            <Link button to="/">
+              Startseite
+            </Link>
+          </div>
         </div>
       </div>
-    </Layout>
+    </LayoutArticle>
   );
 };
 
