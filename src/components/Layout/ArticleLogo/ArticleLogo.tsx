63afe113c16c7c9ad190fed58429309cc78d8bae
Tobias <tobias@fixmycity.de>
2022-06-01 10:10:22 +0200

    LayoutArticle: Improve mobile

--- src/components/Layout/ArticleLogo/ArticleLogo.tsx
+++ src/components/Layout/ArticleLogo/ArticleLogo.tsx
@@ -6,7 +6,7 @@ export const ArticleLogo: React.FC = () => {
     <section className="flex w-full flex-col items-center bg-gradient-to-b from-brand-light-yellow via-brand-light-yellow to-white pb-20 text-gray-700">
       <Link to="/" classNameOverwrite="">
         <img
-          className="my-8 h-12"
+          className="my-5 h-12 lg:my-8"
           src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
           alt="Workflow"
         />
--- src/components/Layout/LayoutArticle.tsx
+++ src/components/Layout/LayoutArticle.tsx
@@ -4,13 +4,22 @@ import { ArticleLogo } from './ArticleLogo';
 
 type Props = {
   location?: any; // TODO: define type
+  hideBetaModal?: boolean;
 };
 
-export const LayoutArticle: React.FC<Props> = ({ location, children }) => {
+export const LayoutArticle: React.FC<Props> = ({
+  location,
+  hideBetaModal,
+  children,
+}) => {
   return (
-    <Layout location={location} className="bg-white">
+    <Layout
+      location={location}
+      hideBetaModal={hideBetaModal}
+      className="bg-white"
+    >
       <ArticleLogo />
-      <article className="prose mx-auto -mt-20 mb-20 max-w-2xl rounded-md bg-white p-10 pt-8">
+      <article className="prose mx-auto -mt-20 mb-20 max-w-2xl bg-white p-5 pt-8 lg:rounded-md lg:p-10">
         {children}
       </article>
     </Layout>
