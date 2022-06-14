8c4472df4ed1663ffe5036481583c9f17a8ef524
Tobias <tobias@fixmycity.de>
2022-06-14 11:58:38 +0200

    Footer: Improve Link Style and Wording

--- src/components/Layout/Footer/FooterLinks/FooterLinkList.tsx
+++ src/components/Layout/Footer/FooterLinks/FooterLinkList.tsx
@@ -15,7 +15,7 @@ export const FooterLinkList: React.FC<Props> = ({ linkList, className }) => {
         <li key={item.name}>
           <Link
             to={item.to}
-            className="block text-base leading-5 text-stone-50 hover:text-white"
+            className="block text-base leading-5 text-stone-50 underline decoration-stone-400 decoration-1 underline-offset-2 hover:text-white hover:decoration-white"
             external={item.external}
           >
             {item.name}
