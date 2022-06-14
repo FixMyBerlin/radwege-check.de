ea3530a050627612588c71c0a1141413fd9177bf
Tobias <tobias@fixmycity.de>
2022-06-14 17:21:59 +0200

    Introduce `/hauptstrassen/alle` page to list all szenes
    Which allows to check the new scene titles easily. And it's also good for SEO to have a link to all pages. (But the page itself is noindex for now.)

--- src/components/Layout/LayoutArticle.tsx
+++ src/components/Layout/LayoutArticle.tsx
@@ -1,16 +1,27 @@
+import classNames from 'classnames'
 import React from 'react'
 import { Layout } from '~/components/Layout/Layout'
 import { ArticleLogo } from './ArticleLogo'
 
 type Props = {
   location?: any // TODO: define type
+  maxWidthClass?: string
 }
 
-export const LayoutArticle: React.FC<Props> = ({ location, children }) => {
+export const LayoutArticle: React.FC<Props> = ({
+  location,
+  maxWidthClass,
+  children,
+}) => {
   return (
     <Layout location={location} className="bg-white">
       <ArticleLogo />
-      <article className="prose mx-auto -mt-20 mb-20 max-w-2xl bg-white p-5 pt-8 lg:rounded-md lg:p-10">
+      <article
+        className={classNames(
+          maxWidthClass ?? 'prose',
+          'mx-auto -mt-20 mb-20 max-w-2xl bg-white p-5 pt-8 lg:rounded-md lg:p-10'
+        )}
+      >
         {children}
       </article>
     </Layout>
