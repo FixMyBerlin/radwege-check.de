c3eea300903117ba8b667a9b33a57f740c938a4b
Tobias <tobias@fixmycity.de>
2022-06-28 14:24:16 +0200

    Results: Remove `allowPressWhenFutureResults` from Facets
    I thought we removed this already, but apparently not for this button type. This still caused issues with situations where buttons where deactivated just because they had the same result count (but different results).

--- src/components/Scenes/Facets/FacetsButtons/ButtonSingleChoice/useResults.ts
+++ src/components/Scenes/Facets/FacetsButtons/ButtonSingleChoice/useResults.ts
@@ -7,15 +7,10 @@ type Props = {
 export const useResults = ({ total, bucketCount, bucketSelected }: Props) => {
   const resultTotal = total
   const resultFuture = bucketCount || 0
-  const resultDiff = resultTotal - resultFuture
 
   const uiSelected = bucketSelected
 
-  const allowPressWhenFutureResults = resultFuture !== 0
-  const allowPreesWhenResultsWouldChange = resultDiff !== 0
-
-  const uiCanpress =
-    allowPressWhenFutureResults && allowPreesWhenResultsWouldChange
+  const uiCanpress = resultFuture !== 0
 
   return { resultTotal, resultFuture, uiSelected, uiCanpress }
 }
