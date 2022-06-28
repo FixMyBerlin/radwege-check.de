303834a49259262dde2127c4a7d01996992137ce
Tobias <tobias@fixmycity.de>
2022-06-28 16:56:55 +0200

    Extract `ScenesAllPage` and apply comp on secondary roads

--- src/pages/hauptstrassen/alle.tsx
+++ src/pages/hauptstrassen/alle.tsx
@@ -1,68 +1,22 @@
-import React, { useMemo } from 'react'
+import React, { useEffect } from 'react'
 import { graphql } from 'gatsby'
-import { LayoutArticle, MetaTags } from '~/components/Layout'
-import { cleanupCsvData, titlePrimaryScene } from '~/components/Scenes/utils'
-import { SceneImage } from '~/components/Scenes'
-import { Link } from '~/components/Link'
-import { FeelSafe } from '~/components/charts'
+import { LayoutArticle } from '~/components/Layout'
+import { ScenesAllPage } from '~/components/ScenesAllPage'
+import { useStore } from 'zustand'
+import { useStoreExperimentData } from '~/components/Scenes/store'
 
 const AllPagePrimary = ({
   data: {
     allScenesPrimaryCsv: { edges: sceneNodes },
   },
 }) => {
-  const scenes = useMemo(() => {
-    // Flatten the data by extracting the objects we want from [node: { /* object */ }, node: { /* object */ }, …]
-    const flattened = sceneNodes.map((list) => list.node)
-    // Clean the data
-    const clean = cleanupCsvData(flattened)
-    return clean.sort((a, b) => a.voteScore - b.voteScore)
-  }, [sceneNodes])
-
-  const totalResults = Number(sceneNodes.length).toLocaleString()
+  const { setExperimentTextKey } = useStore(useStoreExperimentData)
+  useEffect(() => setExperimentTextKey('primary'), [])
 
   return (
     <LayoutArticle maxWidthClass="max-w-full lg:mx-5">
-      <MetaTags
-        article
-        noindex
-        title={`Alle ${totalResults} Hauptstraße-Szenen`}
-        description="Eine lange Übersicht aller Szenen"
-      />
-
-      <h1 className="mb-5 text-center text-4xl font-semibold ">
-        Alle {totalResults} Szenen für Hauptstraßen
-      </h1>
-      <p className="text-center text-gray-500">
-        Aus dem Blickwinkel einer Fahrradfahrer:in. Die Sortierung zeigt die am
-        schlechtesten bewerteten Szenen zuerst.
-        <br />
-        <Link to="/hauptstrassen" button className="mt-2">
-          Ergebnisse filtern…
-        </Link>
-      </p>
-
-      <div className="mt-10 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {scenes.map((scene) => (
-          <Link
-            key={scene.sceneId}
-            to={scene.path}
-            classNameOverwrite="flex flex-col hover:bg-brand-light-yellow hover:shadow-xl p-3 rounded hover:border-gray-100 border-transparent border transition"
-          >
-            <div className="relative">
-              <SceneImage
-                sceneId={scene.sceneId}
-                className="mb-2 rounded object-cover object-bottom"
-              />
-              <div className="absolute top-1 right-1">
-                <FeelSafe value={scene.voteScore} standalone big />
-              </div>
-            </div>
-
-            <h2 className="h-20 leading-tight">{titlePrimaryScene(scene)}</h2>
-          </Link>
-        ))}
-      </div>
+      {/* <MetaTags> are part of <ScenesAllPage> */}
+      <ScenesAllPage rawScenes={sceneNodes} />
     </LayoutArticle>
   )
 }
---
+++ src/pages/nebenstrassen/alle.tsx
@@ -0,0 +1,69 @@
+import { graphql } from 'gatsby'
+import React, { useEffect } from 'react'
+import { useStore } from 'zustand'
+import { LayoutArticle } from '~/components/Layout'
+import { useStoreExperimentData } from '~/components/Scenes/store'
+import { ScenesAllPage } from '~/components/ScenesAllPage'
+import { isProduction } from '~/components/utils'
+import CommingSoon from '../CommingSoon'
+
+const AllPageSecondary = ({
+  data: {
+    allScenesSecondaryCsv: { edges: sceneNodes },
+  },
+}) => {
+  // TEMP deactivated on production while we finish this up
+  if (isProduction) return <CommingSoon />
+
+  const { setExperimentTextKey } = useStore(useStoreExperimentData)
+  useEffect(() => setExperimentTextKey('secondary'), [])
+
+  return (
+    <LayoutArticle maxWidthClass="max-w-full lg:mx-5">
+      {/* <MetaTags> are part of <ScenesAllPage> */}
+      <ScenesAllPage rawScenes={sceneNodes} />
+    </LayoutArticle>
+  )
+}
+
+export default AllPageSecondary
+
+export const query = graphql`
+  query {
+    allScenesSecondaryCsv {
+      edges {
+        node {
+          sceneId
+          sceneIdCar
+
+          parkingCategory
+          carriagewayDirection
+          bicycleStreetType
+          carriagewayWidth
+          carriagewayWidthNumber
+          motorVehicleTrafficVolumen
+
+          voteCarScore
+          voteCar0Unsafe
+          voteCar1RatherUnsafe
+          voteCar2Save
+          voteCar3VerySave
+          voteCarCount
+          voteCarMeans
+
+          voteScore
+          vote0Unsafe
+          vote1RatherUnsafe
+          vote2Save
+          vote3VerySave
+          voteCount
+          voteMeans
+
+          path: gatsbyPath(
+            filePath: "/nebenstrassen/{scenesSecondaryCsv.sceneId}"
+          )
+        }
+      }
+    }
+  }
+`
