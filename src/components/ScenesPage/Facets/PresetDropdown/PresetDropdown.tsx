d7d111b1bcf4a2dd7f88ce56c9f0174a00812a94
Tobias <tobias@fixmycity.de>
2022-07-06 17:48:35 +0200

    Wording "Voreinstellung"

--- src/components/ScenesPage/Facets/PresetDropdown/PresetDropdown.tsx
+++ src/components/ScenesPage/Facets/PresetDropdown/PresetDropdown.tsx
@@ -20,11 +20,14 @@ export const PresetDropdown: React.FC<PresetDropdownProps> = ({
 
   return (
     <Menu as="div" className="relative mb-5 inline-block w-full text-left">
-      <Menu.Button className="inline-flex w-full justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-normal text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+      <Menu.Button
+        className="inline-flex w-full justify-between rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-normal text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+        title={isPreset && presetTitle}
+      >
         <div className="w-full truncate text-left">
           {isCustom && 'Eigene Filterauswahl'}
-          {isPreset && `Filter preset: ${presetTitle}`}
-          {!isPreset && !isCustom && 'Filter preset auswählen'}
+          {isPreset && `Filter Voreinstellung: ${presetTitle}`}
+          {!isPreset && !isCustom && 'Filter Voreinstellung auswählen'}
         </div>
         <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
       </Menu.Button>
