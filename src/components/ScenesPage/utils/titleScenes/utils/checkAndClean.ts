6bbb9fce7df3868389142853a37a9534b86b4ef5
Tobias <tobias@fixmycity.de>
2022-07-14 17:33:56 +0200

    Console error reporting not on production

--- src/components/ScenesPage/utils/titleScenes/utils/checkAndClean.ts
+++ src/components/ScenesPage/utils/titleScenes/utils/checkAndClean.ts
@@ -1,7 +1,10 @@
+import { isProduction } from '~/components/utils'
+
 export const checkAndClean = (inputArray: string[]) => {
   // This informs us of failed text lookups.
   inputArray.forEach((input, index) => {
-    if (input === undefined) {
+    if (input === undefined && !isProduction) {
+      // eslint-disable-next-line no-console
       console.error({
         ERROR:
           'We where not able to pick a text based on the given (now unkonwn) input.',
