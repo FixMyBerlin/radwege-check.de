cc9dfa4cdeeecb7e4dea2bf44a40dcc19a99e158
Tobias <tobias@fixmycity.de>
2022-07-19 12:53:24 +0200

    Home: Improve responsive styling for UsageExample

--- src/components/Homepage/UsageExamples/UsageExample.tsx
+++ src/components/Homepage/UsageExamples/UsageExample.tsx
@@ -15,7 +15,9 @@ export const UsageExample: React.FC<Props> = ({
 }) => {
   return (
     <section className="flex w-full flex-col overflow-hidden border border-neutral-300 lg:flex-row lg:rounded-lg lg:border-neutral-100 lg:shadow-xl">
-      <div className="flex-none lg:w-1/2 lg:pr-3">{image}</div>
+      <div className="flex-none sm:w-60 sm:self-center lg:w-1/2 lg:self-auto lg:pr-3">
+        {image}
+      </div>
       <div className="flex flex-col p-4 pt-0 lg:pt-4">
         <div className="mb-2 flex flex-row items-center justify-between gap-5">
           <h3 className="text-xl font-semibold leading-tight">{title}</h3>
