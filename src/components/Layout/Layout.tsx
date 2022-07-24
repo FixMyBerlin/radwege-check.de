4467d8ff02d49713f7c1a08c2c5e11dc982761a2
Tobias <tobias@fixmycity.de>
2022-07-24 21:17:59 +0200

    Revert "Revert "Remove BetaModal""
    This reverts commit 488b92dd43a37ac9ae1f4cdb0bd5ee38e202db66.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,6 @@
 import classNames from 'classnames'
 import React from 'react'
 import { Footer } from '.'
-import { BetaModal } from './BetaModal'
 import { EnglishLanguageTeaser } from './EnglishLanguageTeaser'
 import { TailwindResponsiveHelper } from './TailwindResponsiveHelper'
 
@@ -9,7 +8,6 @@ type Props = {
   className?: string
   /** @description to access the current location; see links in readme. */
   location?: any // TODO: define type
-  showBetaModal?: boolean
   showEnglishLanguageTeaser?: boolean
   children: React.ReactNode
 }
@@ -18,13 +16,11 @@ type Props = {
 export const Layout: React.FC<Props> = ({
   className,
   location: _location, // TODO later… or remove
-  showBetaModal = false,
   showEnglishLanguageTeaser = true,
   children,
 }) => {
   return (
     <>
-      {showBetaModal && <BetaModal />}
       <main className={classNames(className, 'z-0 flex-grow')}>{children}</main>
       <Footer />
       {showEnglishLanguageTeaser && <EnglishLanguageTeaser />}
